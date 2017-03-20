c9bc6efa4906011e105ea70f914bc4fd1e869e64
jverbosk <jverbosk@gmail.com>
2017-03-20 13:46:56 -0400

    developed methods to handle getting/blocking forks, ready to test more thoroughly

--- sandbox/20.rb
+++ sandbox/20.rb
@@ -9,87 +9,110 @@ opponent = [1, 5, 6]
   def get_potential_wins(wins, forker, forkee)
     potential_wins = []
     wins.each do |win|
-      if (forker & win).size > 0 && (forkee & win).size == 0
-        potential_wins.push(win)
-      end
+      potential_wins.push(win) if (forker & win).size > 0 && (forkee & win).size == 0
     end
-    # p "get_potential_wins: #{potential_wins}"
-    return potential_wins
+    return potential_wins  # [[0, 1, 2], [6, 7, 8], [2, 5, 8]]
   end
 
-  def block_fork(wins, opponent, player)
-    potential_wins = get_potential_wins(wins, opponent, player)
+  def count_positions(wins, forker, forkee)
+    potential_wins = get_potential_wins(wins, forker, forkee)
     position_counts = {}
-    forking_moves = []
     potential_wins.each do |potential_win|
-      # puts "current potential win: #{potential_win}"
       potential_win.each do |position|
-        # puts "current position: #{position}"
         position_counts[position] = 0 if position_counts[position] == nil
         position_counts[position] += 1
       end
     end
+    return position_counts  # {0=>1, 1=>1, 2=>2, 6=>1, 7=>1, 8=>2, 5=>1}
+  end
+
+  def find_fork(wins, forker, forkee)
+    # potential_wins = get_potential_wins(wins, opponent, player)
+    # position_counts = {}
+    position_counts = count_positions(wins, forker, forkee)
+    forking_moves = []
+    # potential_wins.each do |potential_win|
+    #   potential_win.each do |position|
+    #     position_counts[position] = 0 if position_counts[position] == nil
+    #     position_counts[position] += 1
+    #   end
+    # end
     # puts "positions count hash: #{counts}"
-    counts.each do |position, count|
+    position_counts.each do |position, count|
       forking_moves.push(position) if count > 1
     end
     # puts "forking_moves: #{forking_moves}"
-    forking_moves.size > 0 ? forking_moves.sample : get_fork(wins, player, opponent)
+    # forking_moves.size > 0 ? forking_moves.sample : get_fork(wins, player, opponent)
+    return forking_moves  # [2, 8]
   end
 
-  def get_fork(wins, player, opponent)
-    moves = []
-    corner_fork = get_corner_fork(wins, player, opponent)
-    edge_fork = get_edge_fork(wins, player, opponent)
-    center_fork = get_center_fork(wins, player, opponent)
-    if corner_fork.size > 0  # preference corners
-      moves += corner_fork
-    elsif edge_fork.size > 0  # then edges
-      moves += edge_fork
-    elsif center_fork.size > 0  # then center
-      moves += center_fork
+  def check_forks(wins, player, opponent)
+    block_fork = find_fork(wins, opponent, player)
+    get_fork = find_fork(wins, player, opponent)
+    move = nil
+    if block_fork.size > 0
+      move = block_fork.sample
+    elsif get_fork.size > 0
+      move = get_fork.sample
+    else
+      sel_rand(player, opponent)
     end
-    moves.size > 0 ? moves.sample : sel_rand(player, opponent)
+    return move unless move == nil
   end
 
-  def get_corner_fork(wins, player, opponent)
-    matchups = get_matchups(wins, player, opponent)
-    moves_corner = []
-    matchups.each do |matchup|
-      open_corner = (matchup - player) & @corners
-      if open_corner.size > 0  # preference corners
-        moves_corner += open_corner
-      end
-    end
-    p "get_corner_fork: #{moves_corner}"
-    return moves_corner
-  end
+  # def get_fork(wins, player, opponent)
+  #   moves = []
+  #   corner_fork = get_corner_fork(wins, player, opponent)
+  #   edge_fork = get_edge_fork(wins, player, opponent)
+  #   center_fork = get_center_fork(wins, player, opponent)
+  #   if corner_fork.size > 0  # preference corners
+  #     moves += corner_fork
+  #   elsif edge_fork.size > 0  # then edges
+  #     moves += edge_fork
+  #   elsif center_fork.size > 0  # then center
+  #     moves += center_fork
+  #   end
+  #   moves.size > 0 ? moves.sample : sel_rand(player, opponent)
+  # end
 
-  def get_edge_fork(wins, player, opponent)
-    matchups = get_matchups(wins, player, opponent)
-    moves_edge = []
-    matchups.each do |matchup|
-      open_edge = (matchup - player) & @edges
-      if open_edge.size > 0  # preference edge
-        moves_edge += open_edge
-      end
-    end
-    p "get_edge_fork: #{moves_edge}"
-    return moves_edge
-  end
+  # def get_corner_fork(wins, player, opponent)
+  #   matchups = get_matchups(wins, player, opponent)
+  #   moves_corner = []
+  #   matchups.each do |matchup|
+  #     open_corner = (matchup - player) & @corners
+  #     if open_corner.size > 0  # preference corners
+  #       moves_corner += open_corner
+  #     end
+  #   end
+  #   p "get_corner_fork: #{moves_corner}"
+  #   return moves_corner
+  # end
 
-  def get_center_fork(wins, player, opponent)
-    matchups = get_matchups(wins, player, opponent)
-    moves_center = []
-    matchups.each do |matchup|
-      open_center = (matchup - player) & @center
-      if open_center.size > 0  # preference center
-        moves_center += open_center
-      end
-    end
-    p "get_center_fork: #{moves_center}"
-    return moves_center
-  end
+  # def get_edge_fork(wins, player, opponent)
+  #   matchups = get_matchups(wins, player, opponent)
+  #   moves_edge = []
+  #   matchups.each do |matchup|
+  #     open_edge = (matchup - player) & @edges
+  #     if open_edge.size > 0  # preference edge
+  #       moves_edge += open_edge
+  #     end
+  #   end
+  #   p "get_edge_fork: #{moves_edge}"
+  #   return moves_edge
+  # end
+
+  # def get_center_fork(wins, player, opponent)
+  #   matchups = get_matchups(wins, player, opponent)
+  #   moves_center = []
+  #   matchups.each do |matchup|
+  #     open_center = (matchup - player) & @center
+  #     if open_center.size > 0  # preference center
+  #       moves_center += open_center
+  #     end
+  #   end
+  #   p "get_center_fork: #{moves_center}"
+  #   return moves_center
+  # end
 
   # Method to return a random open position, called when no win/block moves in rounds 8 and 9
   def sel_rand(player, opponent)
@@ -98,5 +121,7 @@ opponent = [1, 5, 6]
     position = (all - taken).sample  # take a random open position
   end
 
-
-p block_fork(wins, opponent, player)
+p get_potential_wins(wins, opponent, player)
+p count_positions(wins, opponent, player)
+p find_fork(wins, opponent, player)
+p check_forks(wins, player, opponent)
